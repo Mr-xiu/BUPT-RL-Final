42aaac116767de1062663d027292a17e00c348b1
hyx <1541202878@bupt.edu.cn>
2023-06-21 14:14:53 +0800

    完成play部分

--- play.py
+++ play.py
@@ -1,1 +1,57 @@
 import gymnasium as gym
+from agent import DQNAgent, DDPGAgent
+
+
+def play_dqn(model_path='model/dqn.pth', env_name='MountainCar-v0'):
+    agent = DQNAgent()
+    agent.load_model(model_path=model_path)
+    env = gym.make(env_name, render_mode="human")
+    print('-' * 50 + '\n开始游戏（DQN算法）~')
+    state = env.reset()[0]
+    step = 0  # 走的步数
+    sum_reward = 0
+    while True:
+        env.render()
+        action = agent.DQN.choose_action(state)
+        next_state, reward, terminated, truncated, info = env.step(action)
+        sum_reward += reward
+        step += 1
+        state = next_state
+        if terminated:
+            env.render()
+            print('已到达终点！！！')
+            break
+        elif truncated:
+            print('游戏失败~~~')
+            break
+    print(f'共走了{step}步，获得的总奖励为{sum_reward}~')
+
+
+def play_ddpg(model_path='model/', env_name='MountainCarContinuous-v0'):
+    agent = DDPGAgent()
+    agent.load_model(model_path=model_path)
+    env = gym.make(env_name, render_mode="human")
+    print('-' * 50 + '\n开始游戏（DDPG算法）~')
+    state = env.reset()[0]
+    step = 0  # 走的步数
+    sum_reward = 0
+    while True:
+        env.render()
+        action = agent.DDPG.choose_action(state)
+        next_state, reward, terminated, truncated, info = env.step(action)
+        sum_reward += reward
+        step += 1
+        state = next_state
+        if terminated:
+            env.render()
+            print('已到达终点！！！')
+            break
+        elif truncated:
+            print('游戏失败~~~')
+            break
+    print(f'共走了{step}步，获得的总奖励为{sum_reward}~')
+
+
+if __name__ == '__main__':
+    play_dqn()
+    play_ddpg()
--- train.py
+++ train.py
@@ -1,80 +1,18 @@
-import gymnasium as gym
-import torch
-import numpy as np
-import random
-from buffer import Buffer
-from DQN import DQN
-from tqdm import tqdm
-import matplotlib.pyplot as plt
+from agent import DQNAgent, DDPGAgent
 
 
 def train_dqn():
-    lr = 2e-3
-    num_episodes = 500
-    hidden_dim = 128
-    gamma = 0.98
-    epsilon = 0.01
-    target_update = 10
-    buffer_size = 10000
-    minimal_size = 128
-    batch_size = 64
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
-        "cpu")
+    agent = DQNAgent()
+    agent.fit()
+    agent.show_result()
 
-    env_name = 'MountainCar-v0'
-    env = gym.make(env_name)
-    random.seed(0)
-    np.random.seed(0)
 
-    torch.manual_seed(0)
-    replay_buffer = Buffer(buffer_size)
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.n
-    agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon,
-                target_update, device)
-
-    return_list = []
-    for i in range(100):
-        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
-            for i_episode in range(int(num_episodes / 10)):
-                episode_return = 0
-                state = env.reset()[0]
-                done = False
-                while not done:
-                    action = agent.choose_action(state)
-                    next_state, reward, terminated, truncated, _ = env.step(action)
-                    done = terminated
-                    replay_buffer.add(state, action, reward, next_state, done)
-                    state = next_state
-                    episode_return += reward
-                    # 当buffer数据的数量超过一定值后,才进行Q网络训练
-                    if len(replay_buffer) > minimal_size:
-                        b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
-                        transition_dict = {
-                            'states': b_s,
-                            'actions': b_a,
-                            'next_states': b_ns,
-                            'rewards': b_r,
-                            'dones': b_d
-                        }
-                        agent.update(transition_dict)
-                return_list.append(episode_return)
-                if (i_episode + 1) % 10 == 0:
-                    pbar.set_postfix({
-                        'episode':
-                            '%d' % (num_episodes / 10 * i + i_episode + 1),
-                        'return':
-                            '%.3f' % np.mean(return_list[-10:])
-                    })
-                pbar.update(1)
-
-    episodes_list = list(range(len(return_list)))
-    plt.plot(episodes_list, return_list)
-    plt.xlabel('Episodes')
-    plt.ylabel('Returns')
-    plt.title('DQN on {}'.format(env_name))
-    plt.show()
+def train_ddpg():
+    agent = DDPGAgent()
+    agent.fit()
+    agent.show_result()
 
 
 if __name__ == '__main__':
     train_dqn()
+    train_ddpg()
